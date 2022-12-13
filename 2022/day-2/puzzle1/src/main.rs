a50a7c86bf99750f379040c931b8788aa52adcea
Tom Cahill <me@tomcahill.co.uk>
2022-12-13 08:44:33 +0000

    (WIP) ADDED: Day 2 - Puzzle 1

--- 2022/day-2/puzzle1/src/main.rs
+++ 2022/day-2/puzzle1/src/main.rs
@@ -2,6 +2,35 @@ use std::{fs::File, io::{BufReader, BufRead}};
 
 const FILENAME: &str = "../input.txt";
 
+#[derive(Debug, Copy, Clone)]
+enum HAND {
+    ROCK = 1,
+    PAPER,
+    SCISSORS
+}
+
+// Scoring:
+// Win - 6
+// Draw - 3
+// Loss - 0
+
+fn map_hand(hand_char: &str) -> HAND {
+    match hand_char {
+        "A" | "X" => HAND::ROCK,
+        "B" | "Y" => HAND::PAPER,
+        "C" | "Z" => HAND::SCISSORS,
+        _ => panic!("Uknown hand: {}", hand_char)
+    }
+}
+
+fn check_score(round: Vec<HAND>) -> i32 {
+    let hand_score = round[1] as i8;
+
+    let compare = round[0] as i8 - hand_score;
+
+    0
+}
+
 fn main() {
     let file = match File::open(FILENAME) {
         Ok(file) => file,
@@ -12,10 +41,22 @@ fn main() {
 
     for line in lines {
         if let Ok(value) = line {
-            let parsed =  match value.trim().parse::<String>() {
-                Ok(value) => value,
-                Err(_) => todo!(),
-            };
+            let round = value
+                .as_str()
+                .split_whitespace()
+                .map(map_hand)
+                .collect::<Vec<_>>();
+
+            check_score(round);
+
+            // for round in split {
+            // println!("{:?}", split)
+            // }
+
+            // let scores = split
+            //     .map(map_score);
+
+            // println!("{:?}", split);
         }
 
     }
