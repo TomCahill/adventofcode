aa583ac22cb0c5d3b831dc35339452414f3307af
Tom Cahill <me@tomcahill.co.uk>
2023-12-03 09:33:25 +0000

    ADDED: Day1 complete

--- 2023/src/day-1/puzzle2/index.ts
+++ 2023/src/day-1/puzzle2/index.ts
@@ -1,17 +1,24 @@
 const strDigits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-const strDigitsRegex = new RegExp(`\\d|${strDigits.join('|')}`, 'g');
+const strDigitsRegex = `\\d|${strDigits.join('|')}`;
+const digitsRegexFirst = new RegExp(strDigitsRegex, 'g');
+const digitsRegexLast = new RegExp(`.*(${strDigitsRegex})`, 'g');
 
 export default async function (input: string[]): Promise<number> {
-  return input.reduce((sum: number, line: string): number => {
-    const digits = line.match(strDigitsRegex);
-    if (!digits) throw new Error('No digits found in line');
+  return input.reduce((sum: number, line: string, idx): number => {
+    const [firstMatch] = [...line.matchAll(digitsRegexFirst)];
+    const [lastMatch] = [...line.matchAll(digitsRegexLast)];
 
-    const firstStr = digits.shift() || '';
-    const lastStr = digits.pop() || '';
+    const firstStr = firstMatch[0] || '';
+    const lastStr = lastMatch[1] || '';
 
     const first = (isNaN(Number(firstStr))) ? strDigits.indexOf(firstStr) : Number(firstStr);
     const last = (isNaN(Number(lastStr))) ? strDigits.indexOf(lastStr) : Number(lastStr);
 
-    return sum + Number((first || last) + '' + (last || first));
+    const combined = Number((first || last) + '' + (last || first));
+
+    // const formatted = line.replace(new RegExp(`${firstStr}|${lastStr}`), (x) => `\x1b[4m${x}\x1b[0m`);
+    // console.log(idx+1,':', formatted, '', firstStr, '+', lastStr,'=', combined, ',', 'sum:', sum, 'total:', sum + combined);
+
+    return sum + combined;
   }, 0);
 }
