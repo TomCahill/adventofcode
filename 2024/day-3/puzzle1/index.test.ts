97379ba6535a0d8c760a3829619ac35b7bb60b67
Tom Cahill <me@tomcahill.co.uk>
2025-01-16 13:19:16 +0000

    ADDED: Boilerplate for 2024 D3P1

---
+++ 2024/day-3/puzzle1/index.test.ts
@@ -0,0 +1,6 @@
+import { assertEquals } from "@std/assert";
+import Solve from "./index.ts";
+
+Deno.test(async function D3P1SolveExample() {
+  // Write some puzzle tests.
+});
---
+++ 2024/day-3/puzzle1/index.ts
@@ -0,0 +1,4 @@
+export default async function (input: string[]): Promise<number> {
+  // TODO: Implement puzzle solution
+  return 0;
+}
